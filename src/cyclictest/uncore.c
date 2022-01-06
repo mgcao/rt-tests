7273c5cd4725218d1cba638404bc532e3b15c1a9
Minggui Cao <minggui.cao@intel.com>
2022-01-06 15:24:28 +0800

    modify uncore for WHL/CFL 7 points to sample
    4 CBox event group, 2 ARB and 1 fixed
    
    Signed-off-by: Minggui Cao <minggui.cao@intel.com>

--- src/cyclictest/uncore.c
+++ src/cyclictest/uncore.c
@@ -19,21 +19,25 @@
 #define MSR_UNC_ARB_PERFCTR0_1  0x3B1
 #define MSR_UNC_PERF_FIXED_CTR  0x395
 
+#define GROUP_NUM 4
 #define GROUP_COUNTERS 4
 
 //CBox 0-3, counter0-1, calc total values
-static const uint32_t group_0_counters[GROUP_COUNTERS] = {0x706, 0x716, 0x726, 0x736};
-static const uint32_t group_1_counters[GROUP_COUNTERS] = {0x707, 0x717, 0x727, 0x737};
+static const uint32_t group_counters[GROUP_NUM][GROUP_COUNTERS] = {//{0xc1, 0xc2, 0xc3, 0xc4}};
+                                                                {0x706, 0x716, 0x726, 0x736},
+                                                                {0x707, 0x717, 0x727, 0x737}, 
+                                                                {0x708, 0x718, 0x728, 0x738},
+                                                                {0x709, 0x719, 0x729, 0x739}};
 
-//total calc: group: 2, arb: 2, fixed: 1, 5 kinds of data
-#define UNC_DATA_TYPES 5
+//total calc: group: 4, arb: 2, fixed: 1, 5 kinds of data
+#define UNC_DATA_TYPES 7
 static uint64_t g_unc_start[UNC_DATA_TYPES];
 static uint64_t g_unc_stop[UNC_DATA_TYPES];
 static uint64_t g_unc_delta[UNC_DATA_TYPES];
-static int target_cpu;
+static int target_cpu = 1;
 
-#define read_uncore_msr(reg)  rdmsr_on_cpu(reg, target_cpu)
-#define write_uncore_msr(reg, regval)   wrmsr_on_cpu(reg, target_cpu, regval);
+#define read_uncore_msr(reg)  rdmsr_on_cpu(reg, 1)
+#define write_uncore_msr(reg, regval)   wrmsr_on_cpu(reg, 1, regval);
 
 bool uncore_init(int cpu)
 {
@@ -55,39 +59,37 @@ void uncore_stop_counting(void)
 
 void uncore_pre_read(void)
 {
- //   uncore_start_counting();
 
-    g_unc_start[0] = read_uncore_msr(group_0_counters[0]);
-    g_unc_start[1] = read_uncore_msr(group_1_counters[0]);
-
-    for (int i = 1; i < GROUP_COUNTERS; i++) {
-        g_unc_start[0] += read_uncore_msr(group_0_counters[i]);
-        g_unc_start[1] += read_uncore_msr(group_1_counters[i]);
+    for (int i = 0; i < GROUP_NUM; i++) {
+        g_unc_start[i] = read_uncore_msr(group_counters[i][0]);
+    
+        for (int j = 1; j < GROUP_COUNTERS; j++) {
+            g_unc_start[i] += read_uncore_msr(group_counters[i][j]);
+        }
     }
 
-    g_unc_start[2] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_0);
-    g_unc_start[3] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_1);
+    g_unc_start[4] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_0);
+    g_unc_start[5] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_1);
 
-    g_unc_start[4] = read_uncore_msr(MSR_UNC_PERF_FIXED_CTR);
+    g_unc_start[6] = read_uncore_msr(MSR_UNC_PERF_FIXED_CTR);
 
 }
 
 void uncore_post_read(void)
 {
- //   uncore_stop_counting();
 
-    g_unc_stop[0] = read_uncore_msr(group_0_counters[0]);
-    g_unc_stop[1] = read_uncore_msr(group_1_counters[0]);
-
-    for (int i = 1; i < GROUP_COUNTERS; i++) {
-        g_unc_stop[0] += read_uncore_msr(group_0_counters[i]);
-        g_unc_stop[1] += read_uncore_msr(group_1_counters[i]);
+    for (int i = 0; i < GROUP_NUM; i++) {
+        g_unc_stop[i] = read_uncore_msr(group_counters[i][0]);
+    
+        for (int j = 1; j < GROUP_COUNTERS; j++) {
+            g_unc_stop[i] += read_uncore_msr(group_counters[i][j]);
+        }
     }
 
-    g_unc_stop[2] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_0);
-    g_unc_stop[3] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_1);
+    g_unc_stop[4] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_0);
+    g_unc_stop[5] = read_uncore_msr(MSR_UNC_ARB_PERFCTR0_1);
 
-    g_unc_stop[4] = read_uncore_msr(MSR_UNC_PERF_FIXED_CTR);
+    g_unc_stop[6] = read_uncore_msr(MSR_UNC_PERF_FIXED_CTR);
 
     for (int i = 0; i < UNC_DATA_TYPES; i++) {
         g_unc_delta[i] = g_unc_stop[i] - g_unc_start[i];
